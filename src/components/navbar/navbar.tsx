1c250429e316d48a21a8ed7200a5261a05b05cb5
Jeffrey <jkyyen@gmail.com>
2023-06-16 15:58:06 +0530

    Day 2

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,37 +1,38 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars,  faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import HamburgerMenu from './hamburgermenu';
 
 library.add(faBars, faTimes);
 
 const Navbar = () => {
+  const [isSwitchOn, setSwitchOn] = useState(false);
 
+  const handleToggle = () => {
+    setSwitchOn(!isSwitchOn);
+  };
 
-    const [isSwitchOn, setSwitchOn] = useState(false);
-
-    const handleToggle = () => {
-        setSwitchOn(!isSwitchOn);
-    };
-
-    return (
-        <>
-
-            <nav className={styles.navbar}>
-                <div className={styles.navbaritems}>
-                    <a href='#' className={styles.navbaritem} onClick={() => handleToggle()}><FontAwesomeIcon icon={isSwitchOn ? 'times' : 'bars'} size='2x' />
-                   </a>
-
-                    {isSwitchOn && <HamburgerMenu isOpen={isSwitchOn} />}
-                   
-
-                </div>
-
-            </nav>
-        </>
-    );
-}
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.navbaritems}>
+          <a
+            className={`${styles.navbaritem} ${isSwitchOn ? styles.active : ''}`}
+            onClick={() => handleToggle()}
+          >
+            <FontAwesomeIcon
+              icon={isSwitchOn ? faTimes : faBars}
+              size="2x"
+              className={`${styles.toggleIcon} ${isSwitchOn ? styles.rotate : ''}`}
+            />
+          </a>
+          {isSwitchOn && <HamburgerMenu isOpen={isSwitchOn} />}
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
